7fb78cd231998850513065e59b5b58c4d5b87e6d
mikhail <mikhailbalandin10a@gmail.com>
2024-01-30 10:34:14 +0500

    Logo changed

--- src/app/Ui/Logo/Logo.tsx
+++ src/app/Ui/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
-import LightLogo from '../../../../public/LogoLight.png'
 import DarkLogo from '../../../../public/LogoDark.png'
+import LightLogo from '../../../../public/Logo.png'
 
 export default function Logo({variant} : {variant: string}) {
   return (
@@ -9,7 +9,7 @@ export default function Logo({variant} : {variant: string}) {
       {
         variant === 'light' ?
         <Link href={process.env.FRONTEND_URL !}>
-          <Image src={LightLogo} width={100} height={60} alt="logo"/>
+          <Image src={LightLogo} width={300} height={20} alt="logo"/>
         </Link>
         :
         <Link href={process.env.FRONTEND_URL !}>
