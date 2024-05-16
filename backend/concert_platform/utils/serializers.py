f3566db4239858bf04e3f9964003d79e608859a2
mikhail <mikhailbalandin10a@gmail.com>
2024-05-16 10:17:58 +0500

    fixes

--- backend/concert_platform/utils/serializers.py
+++
@@ -1,8 +0,0 @@
-class ReadWriteSerializerViewSetMixin():
-    read_serializer_class = None
-    write_serializer_class = None
-
-    def get_serializer_class(self):        
-        if self.action in ["create", "update", "partial_update", "destroy"]:
-            return self.write_serializer_class
-        return self.read_serializer_class
